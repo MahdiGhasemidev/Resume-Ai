3f5b691cac9c8ac90b7c41f73b8d94b781479f14
Mahdi Ghasemi <mahdi.ghasemi.dev@gmail.com>
2025-08-17 15:34:38 +0330

    Delete pdf-to-text

--- src/pdf_to_text.py
+++
@@ -1,8 +0,0 @@
-from PyPDF2 import PdfReader
-
-def pdf_to_text(pdf_file):
-    reader = PdfReader(pdf_file)
-    resume_text = ""
-    for page in reader.pages:
-        resume_text += page.extract_text()
-    return resume_text
